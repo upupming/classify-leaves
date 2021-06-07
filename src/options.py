cd8cc40a407533d81c6d078bc1247341e65bccb8
upupming <yimingli.cs@gmail.com>
2021-06-07 19:44:11 +0800

    style: auto format files

--- src/options.py
+++ src/options.py
@@ -1,17 +1,27 @@
 import argparse
 
+
 def getArgs():
-    arg_parser=argparse.ArgumentParser()
-    arg_parser.add_argument("--data_root",type=str,default='../dataset/classify-leaves',
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--data_root", type=str, default='../dataset/classify-leaves',
                             help="the root path of the dataset")
-    arg_parser.add_argument("--val_ratio",type=float,default=0.1,help="the ratio of validation set")
-    arg_parser.add_argument("--epoch",type=int,default=10,help="The number of training epoches")
-    arg_parser.add_argument("--lr",type=float,default=0.1,help="The learning rate of optimizer")
-    arg_parser.add_argument("--batch_size",type=int,default=64,help="The batch size")
-    arg_parser.add_argument("--verbose",action="store_true",help="output the verbose information")
-    arg_parser.add_argument("--model",type=str,help="choose the type of network")
-    arg_parser.add_argument("--freeze",action="store_true",help="freeze the former layers or not")
-    arg_parser.add_argument("--ckpt_path",type=str,default="model.pth",help="the path of the checkpoint")
-    arg_parser.add_argument("--eval_all",action="store_true",help="validate the model after every training epoch")
-    args=arg_parser.parse_args()
+    arg_parser.add_argument("--val_ratio", type=float,
+                            default=0.1, help="the ratio of validation set")
+    arg_parser.add_argument("--epoch", type=int, default=10,
+                            help="The number of training epoches")
+    arg_parser.add_argument("--lr", type=float, default=0.1,
+                            help="The learning rate of optimizer")
+    arg_parser.add_argument("--batch_size", type=int,
+                            default=64, help="The batch size")
+    arg_parser.add_argument("--verbose", action="store_true",
+                            help="output the verbose information")
+    arg_parser.add_argument("--model", type=str,
+                            help="choose the type of network")
+    arg_parser.add_argument("--freeze", action="store_true",
+                            help="freeze the former layers or not")
+    arg_parser.add_argument(
+        "--ckpt_path", type=str, default="model.pth", help="the path of the checkpoint")
+    arg_parser.add_argument("--eval_all", action="store_true",
+                            help="validate the model after every training epoch")
+    args = arg_parser.parse_args()
     return args
